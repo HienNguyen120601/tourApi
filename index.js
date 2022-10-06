7d723a2a2066e141682df74b7e01f7a8fa16e4ca
Hiennguyen120601 <nguyen.nguyenhienth05@gmail.com>
2022-10-07 00:31:14 +0700

    winston,CURD

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const winston = require('winston')
 const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
@@ -10,15 +10,35 @@ var bodyParser = require('body-parser')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(morgan("common"))
 
+
+//create logger
+const logger = winston.createLogger({
+    lavel: 'info',
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize({ all: true })
+            )
+        }),
+        new winston.transports.File({ filename: 'error.log', level: 'error' })
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({ filename: 'exeptions.log' })
+    ]
+})
 
 const bookRouter = require('./router/book')
 
 
-mongoose.connect("mongodb+srv://c2star07:hiennguyen123@cluster0.n0rogqq.mongodb.net/bookdb?retryWrites=true&w=majority").then(() => {
-    console.log("Success connect mongodb")
-})
+
+mongoose.connect("mongodb+srv://c2star07:hiennguyen123@cluster0.n0rogqq.mongodb.net/bookdb?retryWrites=true&w=majority")
+    .then(() => {
+        logger.log("info", "Connected to mongodb")
+    })
+    .catch((err) => {
+        logger.log("error", "Something went wrong", err)
+    })
 app.use('/api/book', bookRouter)
 app.get('/', (req, res) => {
     res.send('helo')
@@ -26,5 +46,5 @@ app.get('/', (req, res) => {
 )
 const PORT = process.env.PORT || 9001
 app.listen(PORT, () => {
-    console.log('Sever is running....', PORT)
+    logger.log("warn", `Sever is running....${PORT}`)
 })
--- router/book.js
+++ router/book.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const { Book, validateBook } = require('../model/books')
+//POST
 router.post('/', async (req, res) => {
     const err = await validateBook(req.body)
     if (err.message)
@@ -24,6 +25,49 @@ router.post('/', async (req, res) => {
         })
 
 
+})
+//GET ALL
+router.get("/", (req, res) => {
+    Book.find().then((book) => res.send(book)).catch((err) => {
+        res.status(500).send("Err")
+    })
+})
+
+//GET by ID
+router.get("/:id", async (req, res) => {
+    const book = await Book.findById(req.params.id)
+    if (!book)
+        res.status(404).send('Not found')
+    else
+        res.send(book)
+})
+
+
+//Update
+router.put("/:id", async (req, res) => {
+    const updateBook = await Book.findByIdAndUpdate(req.params.id, {
+        name: req.body.bookName,
+        author: {
+            name: req.body.authorName,
+            name: req.body.authorDate
+        },
+        genre: req.body.genre
+
+    }, { new: true })
+    if (!updateBook)
+        res.status(404).send("Not found")
+    else
+        res.send(updateBook)
+})
+
+
+//delete
+router.delete("/:id", async (req, res) => {
+    const book = await Book.findByIdAndRemove(req.params.id)
+    if (!book)
+        res.status(404).send("Not found")
+    else
+        res.send(book)
 })
 module.exports = router
 
